e915ad0684ea660ded7496fa610890b48695cd32
김연호 <kimfine000@gmail.com>
2026-05-03 23:30:31 +0900

    feat: 에러, 로딩 처리

--- src/components/post/Posts.tsx
+++ src/components/post/Posts.tsx
@@ -7,12 +7,39 @@ import * as S from "./Post.style.ts";
 const Posts = () => {
 
     const [posts, setPosts] = useState<Post[]>([]);
+    const [error, setError] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
 
     useEffect(() => {
-        getAll().then((data) => {
-            setPosts(data);
-        });
+
+        const fetchPosts = async () => {
+            try {
+                setLoading(true);
+                await new Promise(res => setTimeout(res, 1000));
+                const data = await getAll();
+                setPosts(data);
+            } catch (e: unknown) {
+                console.error(e);
+                setError(true);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchPosts();
     }, []);
+
+    if (error) {
+        return <div>에러 발생</div>
+    }
+
+    if (loading) {
+        return <div>로딩 중...</div>;
+    }
+
+    if (posts.length === 0) {
+        return <div>글이 없습니다</div>;
+    }
 
     return (
         <S.PostsContainer>
