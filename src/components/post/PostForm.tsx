8f4dd7052c472f902356053378e944f529fb9f39
김연호 <kimfine000@gmail.com>
2026-05-06 17:26:55 +0900

    feat: post API 연결

--- src/components/post/PostForm.tsx
+++ src/components/post/PostForm.tsx
@@ -1,19 +1,20 @@
 import * as S from "./Post.style.ts";
-import { useEffect, useState } from "react";
 import * as React from "react";
+import { useState } from "react";
 import type Post from "../../data/post.ts";
 import { uploadPost } from "../../api/api.ts";
+import { useWritingStore } from "../../zustand/useWritingStore.ts";
 
 const PostForm = () => {
-
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
-  const [status, setStatus] = useState<"done" | "loading" | "error">("done");
+  const [status, setStatus] = useState<"done" | "loading" | "error" | "blank">("done");
   const [showDoneMessage, setShowDoneMessage] = useState<boolean>(false);
+  const toggleWriting = useWritingStore(state => state.toggleWriting);
 
   const titleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
-  }
+  };
 
   const contentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setContent(e.target.value);
@@ -25,6 +26,7 @@ const PostForm = () => {
     const trimContent: string = content.trim();
 
     if (trimTitle.length <= 0 || trimTitle.length <= 0) {
+      setStatus("blank");
       return;
     }
 
@@ -36,44 +38,49 @@ const PostForm = () => {
     const fetchPost = async () => {
       try {
         setStatus("loading");
-        await new Promise(res => setTimeout(res, 1000));
-        const data: Post = await uploadPost(post);
+        await uploadPost(post);
         setStatus("done");
         setShowDoneMessage(true);
       } catch (e: unknown) {
         console.error(e);
         setStatus("error");
       }
-    }
+    };
 
     fetchPost();
-  };
 
-  useEffect(() => {
+    const formHtml = e.currentTarget;
+    const titleInputHtml = formHtml.elements.namedItem("title") as HTMLInputElement;
+    const contentInputHtml = formHtml.elements.namedItem("content") as HTMLTextAreaElement;
+    titleInputHtml.value = "";
+    contentInputHtml.value = "";
+
     setTimeout(() => {
       setShowDoneMessage(false);
-    }, 2000);
-  }, [showDoneMessage]);
+      toggleWriting();
+    }, 1800);
+  };
 
   return (
     <S.PostFormContainer>
       <S.PostForm onSubmit={submitForm}>
         <S.PostFormTitle
+          name={"title"}
           type="text"
           placeholder={"제목 입력하기.."}
           onChange={titleChange}
-          required={true}
         />
         <S.PostFormContent
+          name={"content"}
           placeholder={"글 입력하기.."}
           onChange={contentChange}
-          required={true}
         ></S.PostFormContent>
         <S.PostFormSubmitButton>작성</S.PostFormSubmitButton>
       </S.PostForm>
       {status === "error" && <div>에러 발생</div>}
-      {status === "loading" && <div>작성 중...</div>}
-      {showDoneMessage === true && <div color={"white"}>작성 완료!</div>}
+      {status === "blank" && <div>제목 혹은 본문이 비었습니다!</div>}
+      {status === "loading" && <div>업로드 중...</div>}
+      {showDoneMessage === true && <div>업로드 완료!</div>}
     </S.PostFormContainer>
   );
 };
