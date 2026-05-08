ade30714610fdf32c3598da5050206d1e8972879
김연호 <kimfine000@gmail.com>
2026-05-08 13:21:56 +0900

    feat: 포트 변경
    Signed-off-by: 김연호 <kimfine000@gmail.com>

--- src/api/postApi.ts
+++ src/api/postApi.ts
@@ -2,21 +2,21 @@ import type Post from "../types/post.ts";
 
 export async function getAll(): Promise<Post[]> {
   // 1. fetch의 응답을 json으로 변환하면 바로 PostsItem[] 타입의 객체가 됩니다.
-  const response = await fetch("http://localhost:3000/posts");
+  const response = await fetch("http://localhost:4000/posts");
   const data: Post[] = await response.json();
 
   return data;
 }
 
 export async function getOne(id: number): Promise<Post> {
-  const response = await fetch(`http://localhost:3000/posts/${id}`);
+  const response = await fetch(`http://localhost:4000/posts/${id}`);
   const data: Post = await response.json();
 
   return data;
 }
 
 export async function uploadPost(post: Omit<Post, "id">): Promise<Post> {
-  const response = await fetch("http://localhost:3000/posts", {
+  const response = await fetch("http://localhost:4000/posts", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -27,7 +27,7 @@ export async function uploadPost(post: Omit<Post, "id">): Promise<Post> {
 }
 
 export async function deletePost(id: number): Promise<void> {
-  await fetch(`http://localhost:3000/posts/${id}`, {
+  await fetch(`http://localhost:4000/posts/${id}`, {
     method: "DELETE",
   });
 }
